d1facaa6d8019129321caeae18f3e9853ef2467a
Muhammad Nasir <143133415+mnasirEE@users.noreply.github.com>
2024-07-07 23:27:00 +0500

    Delete Nasir/exp3 directory

--- Nasir/exp3/lab1/command_line_args_p3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# taking 2 numbers as command line arguments from terminal
-# calculating sum of them
-# printing their sum on terminal
-firstNumber=$1
-secondNumber=$2
-
-echo "The sum of 2 numbers is: $((firstNumber+secondNumber))"
--- Nasir/exp3/lab1/user_input_p2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# using read for input from user
-
-echo "Enter Your Name: "
-# read command used to get input from user
-read NAME
-echo "$NAME"
