5efca963de242ec65fd6fe1001283cbabbebd5bb
Tayyab <tayyab.hamza@outlook.com>
2024-07-08 11:39:56 +0500

    All files complete

--- Tayyab/Lab3/Ex4.21.sh
+++ Tayyab/Lab3/Ex4.21.sh
@@ -4,8 +4,11 @@
 users=(root user1 user1 user1 user1 user1 user1 user2 user2 user2 root root)
 actions=(login login run copy paste paste logoff login run logoff save logoff)
 
+#clear logfile
+echo "" > logfile.log
+
 for i in {0..11}
 do
-    echo "$(date -I) ${users[i]} ${action[i]}" >> logfile.txt
+    echo "$(date -I) ${users[i]} ${action[i]}" >> logfile.log
 done
 echo "logfile.txt created"
