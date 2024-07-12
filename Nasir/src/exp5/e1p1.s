a854d9d078fc3e1dd9a590a1fb5ba46859a35330
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2024-07-12 16:24:01 +0500

    updated files

--- Nasir/src/exp5/e1p1.s
+++ Nasir/src/exp5/e1p1.s
@@ -4,7 +4,7 @@
 .section .data
     number1: .word 5  # number1 assigned 32 bits - 4 bytes
     number2: .word 15 # number2 assigned 32 bits - 4 bytes
-    absoluteNegative: .word 0x7FFFFFFF # used to convert negative number to positive
+    # absoluteNegative: .word 0x7FFFFFFF # used to convert negative number to positive
 
 # Code section
 .section .text
@@ -14,13 +14,9 @@ _start:
     lw t2, 0(t0)         # x2 = t2 = 5 load value from address x5 in x7
     la t0, number2       # auipc t0, 0(t0) -> addi    t0, t0, 48 - load address of number2 in x5=
     lw t3, 0(t0)         # load value from address x5 in a7
-    sub t4, t2, t3
-    
-    
-           # x9 = x7 - x8
-    bgez t4, end
-
-    neg t4,t4
+    sub t4, t2, t3       # x9 = x7 - x8
+    bgez t4, end         # if t4 >= 0 go to end
+    neg t4,t4            # else takes twos complement
 
 end:
      # Code to exit for Spike (DONT REMOVE IT)
