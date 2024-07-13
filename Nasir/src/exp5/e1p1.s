aff40ad45c6e51b56035ba862012e854cf7dcfcc
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2024-07-13 14:45:48 +0500

    updated code

--- Nasir/src/exp5/e1p1.s
+++ Nasir/src/exp5/e1p1.s
@@ -1,12 +1,13 @@
 .global _start
 
-# data section
+# data section - data memory
 .section .data
-    number1: .word 5  # number1 assigned 32 bits - 4 bytes
-    number2: .word 15 # number2 assigned 32 bits - 4 bytes
+    number1: .word 15  # number1 assigned 32 bits - 4 bytes
+    number2: .word 10 # number2 assigned 32 bits - 4 bytes
+    absoluteDifference: .word  # Variable absolute difference of above two numbers
     # absoluteNegative: .word 0x7FFFFFFF # used to convert negative number to positive
 
-# Code section
+# Code section - instruction memory
 .section .text
 # start the program here
 _start: 
@@ -14,12 +15,14 @@ _start:
     lw t2, 0(t0)         # x2 = t2 = 5 load value from address x5 in x7
     la t0, number2       # auipc t0, 0(t0) -> addi    t0, t0, 48 - load address of number2 in x5=
     lw t3, 0(t0)         # load value from address x5 in a7
+    la t0, absoluteDifference # auipc,t0,0(t0) -> addi t0,t0,offset(address of absolute Difference)
     sub t4, t2, t3       # x9 = x7 - x8
     bgez t4, end         # if t4 >= 0 go to end
-    neg t4,t4            # else takes twos complement
+    neg t4,t4            # sub t4, zero, t4 - else takes twos complement
 
 end:
-     # Code to exit for Spike (DONT REMOVE IT)
+    sw t4, 0(t0) # store absolute diffrence in memory
+    # Code to exit for Spike (DONT REMOVE IT)
     li t0, 1
     la t1, tohost
     sd t0, (t1)
