232395d22cd066603c5e06826f85adf95db88193
Ibrahim1818 <muhammadibraheem23.mi@gmail.com>
2024-07-18 13:45:47 +0500

    Added assembly tasks 1 and 3

--- Ibrahim/src/xce_lab6_assembly/Tasks/task_1.s
+++ Ibrahim/src/xce_lab6_assembly/Tasks/task_1.s
@@ -1,15 +1,12 @@
-.global _start
-
-# data section 
-# data section
 .section .data
-    dividend: .word 20 # 20 will be stored in dividend variable on stack
-    divisor: .word 1   # 3 will be stored in variable divisor
+    dividend: .word 120 # 20 will be stored in dividend variable on stack
+    divisor:  .word 6   # 3 will be stored in variable divisor
     quotient: .word 0  # 0 will be stored in variable quotient
     remainder: .word 0 # 0 will be stored in variable remainder
 # code section
 
 .section .text
+.global _start
 
 _start:
     la t0, dividend  # load address of dividend in t0
@@ -30,22 +27,21 @@ loop:
     srli x11, x11, 31   # store sign bit of dividend in x11
     or t4, t4,x11       # replace lsb of remainder with x11, a sign bit of dividend
     slli t1, t1,1       # shift left dividend by 1 bit
-    addi x12, t4,0          # store remainder in x12 for restoring it 
+    addi x12, t4,0      # store remainder in x12 for restoring it 
     sub t4,t4,t2        # remainder = remainder - divisor
     and x13, t4, t5     # extract sign bit of remainder
     srli x13, x13, 31   # store sign bit of remainder in x13
     beq x13, x10, updateQuotient0RestoreA      # if x13 == x10 =1 then updateQuotient0RestoreA
-    beqz x13, updateQuotientOne                # if x13 == 0 then updateQuotientOne 
-
-updateQuotient0RestoreA:  # A = Remainder
-    slli t3, t3, 1        # update quotient by q[0] = 0
-    addi t4, x12, 0       # restore remainder
-    j decrementLoop       # jump to decrementLoop
 
 updateQuotientOne: 
     slli t3, t3, 1      # shift left
     or t3, t3, 1        # set q[0] = 1
     j decrementLoop     # jump to decrementLoop
+
+updateQuotient0RestoreA:  # A = Remainder
+    slli t3, t3, 1        # update quotient by q[0] = 0
+    addi t4, x12, 0       # restore remainder
+
 
 decrementLoop:
     sub t6, t6, x10  # decrement loop counter by 1
