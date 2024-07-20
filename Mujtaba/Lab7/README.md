a75dc7341eeafb2cf2cc5eddbc85021201202ff7
Muhammad Mujtaba Rawn <mujtaba@uet.edu.pk>
2024-07-20 18:58:11 +0500

    Completed Lab6, Lab7 | Added README Files | Added comments

---
+++ Mujtaba/Lab7/README.md
@@ -0,0 +1,21 @@
+# Python Programming
+
+This folder contains a set of Python exercises aimed to master the python programming concepts.
+
+## Problem 1: Booth Multiplier
+
+- **File:** `Booth_Multiplier.py`
+- **Description:** Implements the multiplication of two signed numbers using Booth Algorithm. You have to change the values to be tested in the above named file.
+
+## Problem 2: Non Restoring Division
+
+- **File:** `non_restore_alg.py`
+- **Description:** Implements the division of two numbers using Non-Restoring Division Algorithm. You have to change the values to be tested in the above named file.
+
+## Task 1: Cache Simulator
+
+- **File:** `cache_simulator`
+- **Description:** Implements the basic Cache Simulator that keep track the misses and hits of cache and show its stats in dictionary format.
+
+# Run the script
+- **To run the scripts:** `./<file_name>` or `python3 <file_name>`
---
+++ Mujtaba/Lab7/cache_simulator.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+class CacheSimulator:
+    def __init__(self, cache_size: int, block_size: int):
+        self.cache_size = cache_size
+        self.block_size = block_size
+        # cache lines
+        self.num_blocks = (int)(cache_size / (block_size/8))
+        # cache memory
+        self.cache = [None] * self.num_blocks
+        self.hits = 0
+        self.misses = 0
+    
+    def read(self, address: int) -> bool:
+        # finding index to which the tag is placed
+        block_index = (address // self.block_size) % self.num_blocks
+        if self.cache[block_index] == address // self.block_size:
+            self.hits += 1
+            print("hit")
+            return True
+        else:
+            self.misses += 1
+            self.cache[block_index] = address // self.block_size
+            print("miss")
+            return False
+    
+    def write(self, address: int):
+        block_index = (address // self.block_size) % self.num_blocks
+        if self.cache[block_index] == address // self.block_size:
+            self.hits += 1
+            print("hit")
+        else:
+            self.misses += 1
+            self.cache[block_index] = address // self.block_size
+            print("miss")
+    
+    def get_stats(self):
+        return {'hits': self.hits, 'misses': self.misses}
+
+# Test Cases
+cache_sim = CacheSimulator(cache_size=1024, block_size=64)
+cache_sim.read(0x0000)  # Expected output: miss
+cache_sim.read(0x0040)  # Expected output: miss
+cache_sim.read(0x0000)  # Expected output: hit
+cache_sim.write(0x0080) # Expected output: miss
+cache_sim.write(0x00C0) # Expected output: miss
+cache_sim.read(0x0080)  # Expected output: hit
+print(cache_sim.get_stats())  # Expected output: {'hits': 2, 'misses': 4}
+
+ 
