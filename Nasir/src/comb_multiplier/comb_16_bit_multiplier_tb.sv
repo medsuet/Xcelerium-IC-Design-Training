cb6289e77e716ee97bf821e3286c6f3f44448f26
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2024-07-24 18:41:07 +0500

    added parameters and added random tests using for loop

--- Nasir/src/comb_multiplier/comb_16_bit_multiplier_tb.sv
+++ Nasir/src/comb_multiplier/comb_16_bit_multiplier_tb.sv
@@ -1,33 +1,76 @@
 module comb_16_bit_multiplier_tb;
-
-    logic [15:0] multiplicand1, multiplier1;
-    logic [31:0] product1;
-
-    localparam period = 10;
-
+// define parameters
+    parameter width = 16, maxNumber = 32768;
+    // define variables for inputs outputs for our main module
+    logic signed [width -1:0] multiplicand1, multiplier1;
+    logic signed [(2*width)-1:0] product1;
+    logic signed [(2*width)-1:0] multiplication;
+    // counter for counting all test cases
+    logic [31:0] counter;
+// module instantiation
     comb_16_bit_multiplier UUT (
         .multiplicand(multiplicand1),
         .multiplier(multiplier1),
         .product(product1)
     );
 
-    initial begin
-        // Test cases
-        multiplicand1 = -16'd5; multiplier1 = 16'd3;
-        #period;
-        multiplicand1 = -16'd230; multiplier1 = -16'd73;
-        #period;
-        multiplicand1 = 16'd67; multiplier1 = -16'd799;
-        #period;
-        multiplicand1 = 16'd5123; multiplier1 = 16'd3678;
-        #period;
-        multiplicand1 = 16'hffff; multiplier1 = 16'hffff;
-        #period;
-        $finish;
+// random Test cases
+// range - 2^15  --- + (2^15 - 1)
+// 32768 - 32767
+// random numbers generator and multiplication by 0, 1, -1
+// Test procedure
+initial begin
+
+    for(int i = 0; i < 10000; i++)begin 
+        // generate random multiplicand and multiplier
+        multiplicand1 = $random % maxNumber;
+        multiplier1 = $random % maxNumber;
+        // calculate reference actual product
+        multiplication = multiplicand1*multiplier1;
+        // delay for collecting custom product
+        // delay added because of propagation delay
+        #1;
+        // displaying all inputs, output and reference product 
+        $display("multiplicand = %0d, multiplier = %d, product = %d, referenceProduct= %d ", multiplicand1, multiplier1, product1, multiplication);
+        // if reference product is equal to our custom product - test passed
+        if (multiplication == product1)begin
+            // displaying which test number passed
+            $display("Test %0d Passed", i);
+        end
+        // if reference product is not equal to our custom product - test failed
+        else begin
+            // displaying which test number failed
+            $display("Test %0d Failed", i);
+        end  
+        counter = i; 
     end
-    
-    initial begin
-        $monitor("multiplicand = %h, multiplier = %h, product = %h", multiplicand1, multiplier1, product1);
+// multiplication by 0, 1, -1
+    for(int i = 0; i < 10000; i++)begin 
+        // generate random multiplicand and multiplier
+        multiplicand1 = $random % maxNumber;
+        // multiplier = 0, 1, -1
+        multiplier1 = $random % 2;
+        // calculate reference actual product
+        multiplication = multiplicand1*multiplier1;
+        // delay for collecting custom product
+        // delay added because of propagation delay
+        #1;
+        // displaying all inputs, output and reference product 
+        $display("multiplicand = %0d, multiplier = %d, product = %d, referenceProduct= %d ", multiplicand1, multiplier1, product1, multiplication);
+        // if reference product is equal to our custom product - test passed
+        if (multiplication == product1)begin
+            // displaying which test number passed
+            $display("Test %0d Passed", i);
+        end
+        // if reference product is not equal to our custom product - test failed
+        else begin
+            // displaying which test number failed
+            $display("Test %0d Failed", i);
+        end  
     end
+    // finish simulation
+    $finish;
+
+end
 
 endmodule
