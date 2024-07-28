a81f614c600d972724c36affeb90c1022508a435
Muhammad Ramzan <mramzansikandar139@gmail.com>
2024-07-28 16:43:40 +0500

    added test bench using task

--- Ramzan/module_08_systemverilog/binary_adder_state_machine/tests/sequential_binaryadder_tb.sv
+++ Ramzan/module_08_systemverilog/binary_adder_state_machine/tests/sequential_binaryadder_tb.sv
@@ -17,41 +17,33 @@ module sequential_binaryadder_tb();
         forever #10 CLK = ~CLK;   //20ns cock period
     end
 
+    task reset;
+        begin
+            RST <= #1 0;
+            repeat(2)@(posedge CLK);
+            RST <= #1 1;
+        end
+    endtask
+
+    task binary_number(input logic [3:0] num);
+        integer i;
+        begin
+            for (i=0 ; i<=3 ; i = i+1) 
+            begin
+                NUMBER <= #1 num[i];
+                @(posedge CLK);
+            end
+            repeat(3)@(posedge CLK);       // Wait for three clock cycles after applying the number
+        end
+    endtask
+
     initial begin
-        RST <= #1 0;
-        repeat(2) @(posedge CLK);      // Wait for two clock cycles
-        RST <= #1 1;
-
-
-        //@(posedge CLK);
-        // verify for 5
-        NUMBER <= #1 1;
-        @(posedge CLK);
-        NUMBER <= #1 0;
-        @(posedge CLK);
-        NUMBER <= #1 1;
-        @(posedge CLK);
-        NUMBER <= #1 0;
-        repeat(3)@(posedge CLK);//repeat for three clock  cycles
-
-        //test case 2
-        //@(posedge CLK);
-        // verify for 8
-        NUMBER <= #1 0;
-        @(posedge CLK);
-        NUMBER <= #1 0;
-        @(posedge CLK);
-        NUMBER <= #1 0;
-        @(posedge CLK);
-        NUMBER <= #1 1;
-        repeat(3)@(posedge CLK);
+        reset();                           // Apply reset
+        binary_number(4'b1010);               // Test case 1: Verify for 5 (0101 in binary) 
+        reset();  
+        binary_number(4'b1000);               // Test case 2: Verify for 8 (1000 in binary)
         $stop;
         $finish;
     end
 endmodule
 
-
-
-
-
-    
