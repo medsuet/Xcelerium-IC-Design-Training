e566023228195aa9332f537a0ede81509eac0731
Bisal Saeed <134628968+codewithbisal@users.noreply.github.com>
2024-07-29 09:36:14 +0500

    Update reverse.s

--- Bisal/assemblycodes/reverse.s
+++ Bisal/assemblycodes/reverse.s
@@ -8,39 +8,37 @@
     .globl _start
 
 _start:
-    # Load the base address of the array into t0
     la t0, array              
-    
-    # Calculate the end address of the array into t1
     li t1, size               
     slli t1, t1, 2            # (shift by 2 so 2^2=4) --> size*4 = total size
     add t1, t0, t1  
     li t3, 4                    
-    sub t1, t1, t3             # point at last element of array
- 
+    sub t1, t1, t3           
     # Loop counter variables 
     li t2, size              
     srli t2, t2, 1            
 
 reverse_loop:
-    # Compare start >= end, break the loop
-    bge t0, t1, end_reverse
-
+    bge t0, t1, done
     # Swap values at t0 and t1
     lw t4, 0(t0)              # t4=t0 --> temporarily store value
     lw t5, 0(t1)              # t5=t1 --> temporarily store value
-    sw t5, 0(t0)              # Store value of t5 in t0 to replace current value with t1
-    sw t4, 0(t1)              # Store value of t4 in t1 to replace current value with t0S
-
+    sw t5, 0(t0)              
+    sw t4, 0(t1)             
     # Change pointers position
     addi t0, t0, 4            # for starting pointer add 4 as each element is of 4 bytes
     addi t1, t1, -4           # for ending pointer substract 4 as each element is of 4 bytes
-
     # check loop condition
     addi t2, t2, -1           
     bnez t2, reverse_loop     # If t2 != 0, repeat the loop
 
-end_reverse:
-    li a7, 10                 # syscall: exit
-    ecall
+done:
+    li t0, 1
+    la t1, tohost
+    sd t0, (t1)
+1:  j 1b
+.section .tohost
+.align 3
+tohost: .dword 0
+fromhost: .dword 0
 
