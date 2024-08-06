fb5285a165a5bf2189ccbe403e0e91f7ea092922
RooshanKhan <rooshan.k12@gmail.com>
2024-08-06 08:28:36 +0500

    IMPROVED

--- Rooshan/Lab_Expermient8/Sequential_Multiplier_Valid_Ready.sv
+++ Rooshan/Lab_Expermient8/Sequential_Multiplier_Valid_Ready.sv
@@ -60,7 +60,7 @@ endmodule
 module Sign_Extension(input logic [15:0]input1,output logic [31:0]output1);
     assign output1={{16{input1[15]}},input1[15:0]};
 endmodule
-module Sequential_Multiplier(input logic clk,input logic reset,input logic start,logic valid_src,logic ready_dst,input logic[15:0]input1,input logic input2_bit,output logic[31:0]Product,output logic End_of_Transmission);
+module Sequential_Multiplier(input logic clk,input logic reset,logic valid_src,logic ready_dst,input logic[15:0]input1,input logic input2_bit,output logic[31:0]Product,output logic End_of_Transmission);
     logic [31:0]Sign_Extended_input1;
     logic [31:0]out_MUX1;
     logic [31:0]out_MUX2;
@@ -76,15 +76,309 @@ module Sequential_Multiplier(input logic clk,input logic reset,input logic start
     assign _2s_complement=(~out_MUX1)+32'b1;
     
     Mux2x1 #(32)mux2(Mux_Sel1,out_MUX1,_2s_complement,out_MUX2);
-    Controller CONTROLLER1(clk,reset,start,valid_src,ready_dst,input2_bit,Mux_Sel1,Mux_Sel2,Summer_enable,End_of_Transmission,shift_amount);
+    Controller CONTROLLER1(clk,reset,valid_src,ready_dst,input2_bit,Mux_Sel1,Mux_Sel2,Summer_enable,End_of_Transmission,shift_amount);
     Shift_Register #(32)SR(shift_amount,out_MUX2,shifted_partial_product);
     Summer #(32)SUMMER1(clk,reset,Summer_enable,End_of_Transmission,shifted_partial_product,Product);
 endmodule
 
+//module Controller(
+//    input logic clk,
+//    input logic reset,
+//    input logic start,
+//    input logic input_bit,
+//    output logic Mux_Sel1,Mux_Sel2,Summer_enable,End_of_Transmission,
+//    output logic [4:0]shift_amount);
+//    logic [4:0] c_state, n_state;
+//    parameter S0=5'd0, S1=5'd1, S2=5'd2,S3=5'd3, S4=5'd4, S5=5'd5,S6=5'd6, S7=5'd7, S8=5'd8,S9=5'd9, S10=5'd10, S11=5'd11,S12=5'd12, S13=5'd13, S14=5'd14,S15=5'd15, S16=5'd16,S17=5'd17;
+//    //state register
+//    always_ff @ (posedge clk or negedge reset)
+//    begin
+//    //reset is active low
+//    if (!reset)
+//    c_state <= S0;
+//    else
+//    c_state <= n_state;
+//    end
+//    //next_state always block
+//    always_comb
+//    begin
+//    case (c_state)
+//        S0: begin if (start) n_state = S1;
+//        else n_state = S0; end
+//        S1: n_state = S2;
+//        S2: n_state = S3;
+//        S3: n_state = S4;
+//        S4: n_state = S5;
+//        S5: n_state = S6;
+//        S6: n_state = S7;
+//        S7: n_state = S8;
+//        S8: n_state = S9;
+//        S9: n_state = S10;
+//        S10: n_state = S11;
+//        S11: n_state = S12;
+//        S12: n_state = S13;
+//        S13: n_state = S14;
+//        S14: n_state = S15;
+//        S15: n_state = S16;
+//        S16: n_state = S17;
+//        S17: n_state = S0;
+//        default: n_state = S0;
+//        endcase
+//    end
+//    //output always block
+//    always_comb
+//    begin
+//        case (c_state)
+//                S0:begin shift_amount = 0;
+//                    Mux_Sel1=0;
+//                    Mux_Sel2=1;
+//                    Summer_enable=0;
+//                    End_of_Transmission=0;
+//                end
+//            S1:begin shift_amount = 0;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S2:begin shift_amount = 1;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S3:begin shift_amount = 2;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end            
+//            S4:begin shift_amount = 3;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S5:begin shift_amount = 4;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S6:begin shift_amount = 5;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S7:begin shift_amount = 6;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end            
+//            S8:begin shift_amount = 7;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S9:begin shift_amount = 8;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S10:begin shift_amount = 9;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S11:begin shift_amount = 10;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end            
+//            S12:begin shift_amount = 11;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S13:begin shift_amount = 12;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S14:begin shift_amount = 13;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S15:begin shift_amount = 14;
+//                Mux_Sel1=0;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end            
+//            S16:begin shift_amount = 15;
+//                Mux_Sel1=1;
+//                Summer_enable=1;
+//                End_of_Transmission=0;
+//                if (input_bit)begin
+//                    Mux_Sel2=0;
+//                end
+//                else begin
+//                    Mux_Sel2=1;
+//                end
+//            end
+//            S17:begin shift_amount = 0;
+//                Mux_Sel1=0;
+//                Mux_Sel2=1;
+//                Summer_enable=0;
+//                End_of_Transmission=1;
+//            end
+//            default:begin shift_amount = 0;
+//                Mux_Sel1=0;
+//                Mux_Sel2=1;
+//                Summer_enable=0;
+//                End_of_Transmission=0;
+//            end
+//        endcase
+//    end
+//endmodule
+
+//module Half_Adder(input logic bit1,input logic bit2,output logic Sum,output logic Carry_out
+//);
+//    always_comb begin
+//        if ((bit1&bit2)==1)begin
+//            Carry_out=1;
+//            Sum=0;
+//        end
+//        else begin
+//            Carry_out=0;
+//            Sum=bit1|bit2;
+//        end
+//    end
+//endmodule
+//module Full_Adder(input logic bit1,input logic bit2,input logic Carry_in,output logic Sum,output logic Carry_out
+//);
+//    always_comb begin
+//        if (((bit1&bit2)==1)|((bit1&Carry_in)==1)|((bit2&Carry_in)==1))begin
+//            Carry_out=1;
+//            Sum=bit1&bit2&Carry_in;
+//        end
+//        else begin
+//            Carry_out=0;
+//            Sum=bit1|bit2|Carry_in;
+//        end
+//    end
+//endmodule
+
+//module _32_Bit_Adder(input logic [15:0]Value1,input logic [15:0]Value2,output logic [15:0]Sum
+//);
+//    logic [15:0]Carry_out;
+//    Half_Adder HA0x(Value1[0],Value2[0],Sum[0],Carry_out[0]);
+//    Full_Adder FA1x(Value1[1],Value2[1],Carry_out[0],Sum[1],Carry_out[1]);
+//    Full_Adder FA2x(Value1[2],Value2[2],Carry_out[1],Sum[2],Carry_out[2]);
+//    Full_Adder FA3x(Value1[3],Value2[3],Carry_out[2],Sum[3],Carry_out[3]);
+//    Full_Adder FA4x(Value1[4],Value2[4],Carry_out[3],Sum[4],Carry_out[4]);
+//    Full_Adder FA5x(Value1[5],Value2[5],Carry_out[4],Sum[5],Carry_out[5]);
+//    Full_Adder FA6x(Value1[6],Value2[6],Carry_out[5],Sum[6],Carry_out[6]);
+//    Full_Adder FA7x(Value1[7],Value2[7],Carry_out[6],Sum[7],Carry_out[7]);
+//    Full_Adder FA8x(Value1[8],Value2[8],Carry_out[7],Sum[8],Carry_out[8]);
+//    Full_Adder FA9x(Value1[9],Value2[9],Carry_out[8],Sum[9],Carry_out[9]);
+//    Full_Adder FA10x(Value1[10],Value2[10],Carry_out[9],Sum[10],Carry_out[10]);
+//    Full_Adder FA11x(Value1[11],Value2[11],Carry_out[10],Sum[11],Carry_out[11]);
+//    Full_Adder FA12x(Value1[12],Value2[12],Carry_out[11],Sum[12],Carry_out[12]);
+//    Full_Adder FA13x(Value1[13],Value2[13],Carry_out[12],Sum[13],Carry_out[13]);
+//    Full_Adder FA14x(Value1[14],Value2[14],Carry_out[13],Sum[14],Carry_out[14]);
+//    Full_Adder FA15x(Value1[15],Value2[15],Carry_out[14],Sum[15],Carry_out[15]);
+
+//endmodule
 module Controller(
     input logic clk,
     input logic reset,
-    input logic start,valid_src,ready_dst,
+    input logic valid_src,ready_dst,
     input logic input_bit,
     output logic Mux_Sel1,Mux_Sel2,Summer_enable,End_of_Transmission,
     output logic [4:0]shift_amount);
@@ -104,7 +398,7 @@ module Controller(
     always_comb
     begin
     case (c_state)
-        S0: begin if (start && valid_src && ready_src) n_state = S1;
+        S0: begin if (valid_src && ready_src) n_state = S1;
         else n_state = S0; end
         S1: n_state = S2;
         S2: n_state = S3;
@@ -122,7 +416,7 @@ module Controller(
         S14: n_state = S15;
         S15: n_state = S16;
         S16: n_state = S17;
-        S17: begin if (End_of_Transmission) n_state = S0;
+        S17: begin if (valid_dst&&ready_dst) n_state = S0;
         else n_state = S17;end
         default: n_state = S0;
         endcase
@@ -367,6 +661,7 @@ module Controller(
                 Summer_enable=0;
                 End_of_Transmission=0;
                 ready_src=0;
+                valid_dst=0;
             end
         endcase
     end
--- Rooshan/Lab_Expermient8/Sequential_Multiplier_Valid_Ready_TB.sv
+++ Rooshan/Lab_Expermient8/Sequential_Multiplier_Valid_Ready_TB.sv
@@ -5,50 +5,47 @@ module Sequential_Multiplier_TB();
     logic reset,start;
     logic bit_val,valid_src,ready_dst,End_of_Transmission;
     logic [31:0]result;
-    int c=0;
     logic [15:0]VALUE1;
     logic [15:0]VALUE2;
-    Sequential_Multiplier DUT(.clk(clk),.reset(reset),.start(start),.valid_src(valid_src),.ready_dst(ready_dst),.input1(VALUE1),.input2_bit(bit_val),.Product(result),.End_of_Transmission(End_of_Transmission));
-    task driver(input logic [15:0]driver_Value1,input logic [15:0]driver_Value2,input logic driver_valid_src,input logic driver_ready_dst,input logic driver_start);
+    Sequential_Multiplier DUT(.clk(clk),.reset(reset),.valid_src(valid_src),.ready_dst(ready_dst),.input1(VALUE1),.input2_bit(bit_val),.Product(result),.End_of_Transmission(End_of_Transmission));
+    task driver(input logic [15:0]driver_Value1,input logic [15:0]driver_Value2,input logic driver_valid_src,input logic driver_ready_dst);
     begin
         VALUE1=driver_Value1;
         VALUE2=driver_Value2;
         valid_src=driver_valid_src;
         ready_dst=driver_ready_dst;
-        start = driver_start;
         while(valid_src!=1)begin
-            #1;
+            @(posedge clk);
         end
         for(int i=0;i<16;i++)begin
             @(posedge clk);
             bit_val=VALUE2[i];
-            start = 0;
+            valid_src = 0;
         end
     end
     endtask
     task monitor;
     begin
-//        while(1)begin
+    forever while(1)begin
         if (ready_dst)begin
             @(negedge clk)
             #1;
-            $display("%0t : The product of %0d and %0d is %0d which is equal to the result %0d(through direct multiplication)",$time,$signed(VALUE1),$signed(VALUE2),$signed(result),$signed(VALUE1)*$signed(VALUE2));
             if ($signed(result)!=$signed(VALUE1)*$signed(VALUE2))begin
                 $display("The product of %d and %d is not %d but %d",$signed(VALUE1),$signed(VALUE2),$signed(result),$signed(VALUE1)*$signed(VALUE2));
-                c=c+1;
             end
+            break;
         end
-//        end
+        end
     end
     endtask
     always #(CLK_PERIOD/2) clk = ~clk;
     initial begin
-        reset = 0;
-        #1;
         reset = 1;
-        #1;
-        driver($random()%30,$random()%30,0,0,1);
-        driver($random()%60,$random()%60,0,0,1);
+        #10;
+        reset = 0;
+        #10;
+        driver($random()%30,$random()%30,0,0);
+        driver($random()%60,$random()%60,0,0);
         #5000
         $finish;
     end
