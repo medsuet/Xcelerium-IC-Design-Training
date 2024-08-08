b9f41eeeaf41ba2456706db8daf2e29772bf3c6d
Fazail333 <fazailbutt25@gmail.com>
2024-08-08 22:14:01 +0500

    add the two drivers and monitor in the rdy-valid sequential multiplier testbench

--- Fazail/assignments/rdy_val_sequential_multiplier/test/tb_sequential_multiplier.sv
+++ Fazail/assignments/rdy_val_sequential_multiplier/test/tb_sequential_multiplier.sv
@@ -13,7 +13,7 @@ logic signed [WIDTH-1:0] multiplicand, m_in2;
 // Output signal
 logic signed [2*WIDTH-1:0] product;
 
-// Val-Ready protoccol
+// Val-ready protoccol
 logic src_valid_i, dst_ready_i;
 logic src_ready_o, dst_valid_o;
 
@@ -23,6 +23,7 @@ logic src_ready_o, dst_valid_o;
 logic n_rst;
 
 int fd; // file variable
+int tests = 10;
 
 sequential_multiplier UUT (
     .multiplier (multiplier), .multiplicand(multiplicand),
@@ -46,59 +47,82 @@ initial begin
 
     /* ---> Direct Test <--- */
     // every test takes atleast 16-18 cycles
-    /*direct_test (16'hFFFF, 16'hFFFF);
+    direct_test (16'hFFFF, 16'hFFFF);
     direct_test (16'h8000, 16'hFFFF);
     direct_test (16'h90, 16'h8000);
-    direct_test (16'h8001, 16'h1);*/
+    direct_test (16'h8001, 16'h1);
 
     reset_seq;
 
     fork
-        driver(2);
-        monitor(2);  
+        driver_src(tests);
+        driver_dst(tests);
+        monitor(tests);  
     join
 
     @(posedge clk);
-    $stop;
+    //$stop;
     $finish;
 end
 
-task driver (input int tests);
+task driver_src (input int tests);
     begin
-        for (int j = 0;j <= tests ; j++) begin
-            multiplier = $random();
-            multiplicand = $random();
-
-            src_ready_before_valid;
-            dst_ready_before_valid;
+        for (int j = 1; j < tests ; j++) begin
+            multiplier <= $random();
+            multiplicand <= $random();
+            src_valid_i <= 1;
+            while(!src_ready_o) begin
+                @(posedge clk);
+            end
+            @(posedge clk);
+            src_valid_i = 0;
+            repeat(1) @(posedge clk);
         end
     end
 endtask
 
+task driver_dst (input int tests);
+    begin
+        for (int k = 1; k < tests; k++) begin
+            dst_ready_i <= 1;
+            while (!dst_valid_o) begin
+                @(posedge clk);
+            end
+            @(posedge clk); 
+            dst_ready_i = 0;
+            @(posedge clk); 
+        end
+    end 
+endtask 
+
 task monitor(input int tests);
     fd = $fopen ("log/output_result.log", "w");
 
-    for(int i = 1; i <= tests; i++) begin
-        @(posedge src_valid_i);
-        m_in = multiplier;
-        m_in2 = multiplicand;
+    for(int i = 1; i < tests; i++) begin
         
-        while (!dst_valid_o) begin
+        while (!(src_ready_o & src_valid_i)) begin
             @(posedge clk);
         end
 
+        m_in <= multiplier;
+        m_in2 <= multiplicand;
+        
+        while (!(dst_valid_o & dst_ready_i)) begin
+            @(posedge clk);
+        end
+        
         // Self Check 
         if (mul_ref(m_in, m_in2) == product) begin
             $fdisplay(fd,"PASS: multiplier = %d, multiplicand = %d | product = %d ", multiplier, multiplicand, product);
-            //$display("Test No. : %0d PASS: multiplier = %d, multiplicand = %d | product = %d ",i ,multiplier, multiplicand, product);
+            $display("Test No. : %0d PASS: multiplier = %d, multiplicand = %d | product = %d ",i ,multiplier, multiplicand, product);
         end
         else begin
             $fdisplay(fd,"FAIL: multiplier = %0d, multiplicand = %0d, product = %0d, expected_value = %0d ", multiplier,
             multiplicand, product, mul_ref(m_in, m_in2));
             $display("FAIL: multiplier = %0d, multiplicand = %0d, product = %0d, expected_value = %0d ", multiplier,
             multiplicand, product, mul_ref(m_in, m_in2));
+            $finish;
         end
- 
     end
     $fclose(fd);
 endtask
@@ -108,36 +132,15 @@ task init_signals;
     multiplier = 0; multiplicand = 0;
 
     src_valid_i = 0; dst_ready_i = 0;
-    src_ready_o = 0; dst_valid_o =0;
+    src_ready_o = 0; dst_valid_o = 0;
     @(posedge clk);
 endtask 
 
 task reset_seq;
-    n_rst = 0;
+    n_rst <= 0;
     @(posedge clk); 
-    n_rst = 1;
+    n_rst <= 1;
     @(posedge clk);
-endtask
-
-// When reset is applied source-ready is on
-task src_ready_before_valid;
-    @(posedge clk);
-    src_valid_i = 1;
-    while (!src_ready_o)begin
-        @(posedge clk);
-    end
-    @(posedge clk);
-    src_valid_i = 0;
-endtask
-
-task dst_ready_before_valid;
-    @(posedge clk);
-    dst_ready_i = 1;
-    @(posedge clk);
-    while (!dst_valid_o) begin
-        @(posedge clk);        
-    end
-    dst_ready_i = 0;
 endtask
     
 
@@ -146,8 +149,22 @@ task direct_test(input signed [WIDTH-1:0] in_a, input signed [WIDTH-1:0] in_b);
     reset_seq;
     multiplier =  in_a; multiplicand = in_b;
 
-    src_ready_before_valid;
-    dst_ready_before_valid;
+    src_valid_i <= 1;
+    //@(posedge clk);
+    while(!src_ready_o) begin
+        @(posedge clk);
+        $display("src");
+    end
+    @(posedge clk);
+    src_valid_i = 0;
+    repeat(0) @(posedge clk);
+
+    dst_ready_i <= 1;
+    @(posedge clk); 
+    while (!dst_valid_o) begin
+        @(posedge clk);
+    end
+    dst_ready_i = 0;
 
     // Self Check 
     if (mul_ref(in_a, in_b) == product) begin
